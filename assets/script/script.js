7ef467f5c9ec8aeb98e6c2aa8ce2aa56e31f16bc
Ollie-Judge <ollie.judge@hotmail.com>
2021-11-23 12:34:23 +0000

    added explanations for each line up to this point and added a key press function that starts the game when any keyboard button is pushed

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,36 +1,49 @@
 var colours = ["pink", "yellow", "lightblue", "brown"]
 
-var pattern = [];
+var pattern = []; /* randomly generated pattern */
 
-chosenPattern = [];
+chosenPattern = []; /* user selected pattern */
+
+var begun = false; /* stops the game from just starting on its own */
+var level = 0; /* sets the game level to 0 at the beginning and changes as the game progresses */
+
+$(document).keypress(function() {
+    if(!begun) {
+        $("#gameText").text("Level " + level); /* when any key on the keyboard gets pressed this function activates, 
+        which runs this line here which changes the "gameText" element content to display the level youre on instead of start instructions */
+        ensuingPattern();
+        begun = true;
+    }
+});
+
 
 $('.btn').click(function() {
 
-    var selectedColour = $(this).attr("id")
-
-    chosenPattern.push(selectedColour);
-
-    animateClick(selectedColour);
+    var selectedColour = $(this).attr("id") /* chooses the id attribute in the div that has been selected by the user */
+    chosenPattern.push(selectedColour); /* pushes that chosen div to an empty array called "selected colour" */
+    animateClick(selectedColour); /* connects up teh animation in the function called "animateClick" top the click function we're in here */
 
 });
 
 function ensuingPattern() {
 
-    var randomNumber = Math.floor(Math.random() * 4);
+    level++; /* incrementally progresses the game */
+    $("#gameText").text("Level " + level);
 
-    var randomColour = colours[randomNumber];
+    var randomNumber = Math.floor(Math.random() * 4); /* Generates random number between 0 and 3 */
+    var randomColour = colours[randomNumber]; /* Grabs the random number from above and corresponds it to the array, 
+    then picks 1 of the 4 colours in the array, producing that instead of the number */
+    pattern.push(randomColour); /* pushes the random colour to the empty array called "pattern" */
 
-    pattern.push(randomColour);
-
-    $('#' + randomColour).fadeIn(150).fadeOut(150).fadeIn(150);
+    $('#' + randomColour).fadeIn(150).fadeOut(150).fadeIn(150); /* sets animation speeds for the random colour */
 
 }
 
 function animateClick(chosenColour) {
 
-    $('#' + chosenColour).addClass('chosen');
+    $('#' + chosenColour).addClass('chosen'); /* looks at the class of the chosen div and adds the class "chosen" to it */
 
     setTimeout(function() {
-        $('#' + chosenColour).removeClass('chosen');
-    }, 150);
+        $('#' + chosenColour).removeClass('chosen'); /* removes the "chosen" class from the div selected by the user */
+    }, 150); /* Gives the animation a speed at which it removes the class */
 }
