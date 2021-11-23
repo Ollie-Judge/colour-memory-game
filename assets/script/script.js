36f12762918927e27f3d0b7e6bcfb5fbd7b83dd0
Ollie-Judge <ollie.judge@hotmail.com>
2021-11-23 11:53:04 +0000

    added click function that knows which button has been clicked

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,11 +2,24 @@ var colours = ["pink", "yellow", "lightblue", "brown"]
 
 var pattern = [];
 
+chosenPattern = [];
+
+$('.btn').click(function() {
+
+    var selectedColour = $(this).attr("id")
+
+    chosenPattern.push(selectedColour);
+
+});
+
 function ensuingPattern() {
+
     var randomNumber = Math.floor(Math.random() * 4);
 
     var randomColour = colours[randomNumber];
 
     pattern.push(randomColour);
 
+    $('#' + randomColour).fadeIn(150).fadeOut(150).fadeIn(150);
+    
 }
